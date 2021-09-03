49c2e6669e97e4c18b75c497bc0efd5cd0d3d7be
Franklyn Roberto <franklyn1998@live.com>
2021-09-03 17:50:09 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Position position = new Position(10, 10);
+		System.out.println(position);
+		
 	}
 
 }
